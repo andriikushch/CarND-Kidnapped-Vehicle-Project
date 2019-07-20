122f6bd085102df68b57e0a203213d96b6decd4b
Andrii Kushch <andrii.kushch@gmail.com>
2019-07-20 19:00:30 +0200

    resample function is implemented

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -209,7 +209,29 @@ void ParticleFilter::resample() {
    * NOTE: You may find std::discrete_distribution helpful here.
    *   http://en.cppreference.com/w/cpp/numeric/random/discrete_distribution
    */
+  vector<Particle> resampled_particles;
+  std::default_random_engine gen;
 
+  std::uniform_int_distribution<int> particle_index(0, num_particles - 1);
+
+  int current_index = particle_index(gen);
+
+  double beta = 0.0;
+
+  double max_weight = *max_element(std::begin(weights), std::end(weights));
+
+  for (int i = 0; i < particles.size(); i++) {
+    std::uniform_real_distribution<double> random_weight(0.0, max_weight * 2);
+    beta += random_weight(gen);
+
+    while (beta > weights[current_index]) {
+      beta -= weights[current_index];
+      current_index = (current_index + 1) % num_particles;
+    }
+    resampled_particles.push_back(particles[current_index]);
+  }
+
+  particles = resampled_particles;
 }
 
 void ParticleFilter::SetAssociations(Particle& particle, 
